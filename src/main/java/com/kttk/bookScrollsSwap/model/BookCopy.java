b48f7220a1eea72afb7016c50960f14bd75df747
Fastus001 <tkarczmarzyk@gmail.com>
2021-03-06 11:10:42 +0100

    Created Book, BookCopy and Author entities. #9

--- src/main/java/com/kttk/bookScrollsSwap/model/BookCopy.java
+++ src/main/java/com/kttk/bookScrollsSwap/model/BookCopy.java
@@ -2,13 +2,27 @@ package com.kttk.bookScrollsSwap.model;
 
 import com.kttk.bookScrollsSwap.enums.BookQuality;
 import com.kttk.bookScrollsSwap.enums.BookState;
+import lombok.Data;
+
+import javax.persistence.*;
 
 /**
  * Created by Tom - 06.03.2021
  */
+@Data
+@Entity
 public class BookCopy {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
     private Book book;
+
+    @Enumerated(value = EnumType.STRING)
     private BookState state;
+
+    @Enumerated(value = EnumType.STRING)
     private BookQuality quality;
 }
