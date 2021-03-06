1d23b33c476106e7012322edbd3c9825005c2603
kattommy <katarzynaa.nowak@wp.pl>
2021-03-06 10:29:55 +0100

    add User class

--- src/main/java/com/kttk/bookScrollsSwap/model/User.java
+++ src/main/java/com/kttk/bookScrollsSwap/model/User.java
@@ -1,4 +1,16 @@
 package com.kttk.bookScrollsSwap.model;
 
+import lombok.Data;
+import org.hibernate.type.LocalDateType;
+@Data
 public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDateType registrationDate;
+    private String status;
+
 }
